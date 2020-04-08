eab0dd6a38d1401c978802e520333e47586f7578
Kenta Iwasaki <kenta@lithdew.net>
2020-04-08 22:45:37 +0900

    config: cleanup funcs s.t. config may not only be declared via starlark

--- config.go
+++ config.go
@@ -6,38 +6,33 @@ import (
 )
 
 type Config struct {
-	main     *starlark.Thread
-	vars     starlark.StringDict
 	handlers map[string]func()
-
-	program *starlark.Program
 }
 
-func (c *Config) declareBuiltins() {
-	c.vars["sql"] = starlark.NewBuiltin("sql", SQL)
-	c.vars["http_get"] = starlark.NewBuiltin("http_get", GET)
+func (c *Config) declareStarlarkBuiltins() starlark.StringDict {
+	return starlark.StringDict{
+		"sql":      starlark.NewBuiltin("sql", SQL),
+		"http_get": starlark.NewBuiltin("http_get", GET),
+	}
+}
+
+func (c *Config) parseStarlark(src interface{}) error {
+	vars := c.declareStarlarkBuiltins()
+
+	_, program, err := starlark.SourceProgram("", src, vars.Has)
+	if err != nil {
+		return fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	_, err = program.Init(&starlark.Thread{Name: "main"}, vars)
+	if err != nil {
+		return fmt.Errorf("failed to init config: %w", err)
+	}
+
+	return nil
 }
 
 func LoadConfig(src interface{}) (*Config, error) {
-	cfg := &Config{
-		main:     &starlark.Thread{Name: "main"},
-		vars:     make(starlark.StringDict),
-		handlers: make(map[string]func()),
-	}
-
-	cfg.declareBuiltins()
-
-	_, program, err := starlark.SourceProgram("", src, cfg.vars.Has)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse config: %w", err)
-	}
-
-	cfg.program = program
-
-	_, err = program.Init(cfg.main, cfg.vars)
-	if err != nil {
-		return nil, fmt.Errorf("failed to init config: %w", err)
-	}
-
-	return cfg, nil
+	cfg := &Config{handlers: make(map[string]func())}
+	return cfg, cfg.parseStarlark(src)
 }
